e883c51dcd1ace9bd08a0c3dd13f03b17c1b7cf7
David Manvel <manvelyancda@masterscada.ru>
2025-05-02 10:25:47 +0300

    add license and readme

--- main.ts
+++ main.ts
@@ -3,9 +3,9 @@ const encoder = new TextEncoder();
 const hosts = new Map<string, string>();
 
 const udpListener = Deno.listenDatagram({
-  hostname:"0.0.0.0",
+  hostname: "0.0.0.0",
   port: 10000,
-  transport: "udp"
+  transport: "udp",
 });
 
 if (import.meta.main) {
@@ -14,14 +14,15 @@ if (import.meta.main) {
   await Promise.all([
     listen(),
     send(1000),
-    view(2000)
+    view(2000),
   ]);
 }
 
 async function view(interval: number) {
-  while(true){
-    for(const address of hosts.keys())
+  while (true) {
+    for (const address of hosts.keys()) {
       console.log(`${address}\t${hosts.get(address)}`);
+    }
 
     hosts.clear();
     await delay(interval);
@@ -29,25 +30,26 @@ async function view(interval: number) {
   }
 }
 
-async function listen() : Promise<void> {
-  for await (const [data, address] of udpListener)
+async function listen(): Promise<void> {
+  for await (const [data, address] of udpListener) {
     hosts.set((address as Deno.NetAddr).hostname, decoder.decode(data));
+  }
 }
 
-async function send(interval: number) : Promise<void>{
-  while(true){
+async function send(interval: number): Promise<void> {
+  while (true) {
     await udpListener.send(
-      encoder.encode(Deno.hostname()), 
+      encoder.encode(Deno.hostname()),
       {
-        hostname:"255.255.255.255",
+        hostname: "255.255.255.255",
         port: 10000,
         transport: "udp",
-      }
+      },
     );
     await delay(interval);
   }
 }
 
-async function delay(ms:number) {
-  return await new Promise(resolve => setTimeout(resolve, ms));
+async function delay(ms: number) {
+  return await new Promise((resolve) => setTimeout(resolve, ms));
 }
---
+++ readme.md
@@ -0,0 +1,10 @@
+# LanFinder
+
+this is a simpe app built with Deno to find devices in one lan network with udp
+broadcasting
+
+## Deno
+
+run: `deno task dev`
+
+compile: `deno task build`
